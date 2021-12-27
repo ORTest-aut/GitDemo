5b4b9ce032dc1bc79df53a7da81454f6d8d23e4f
reakh <olivier.ricaud@yahoo.fr>
2021-12-27 17:32:39 +0100

    first commit

---
+++ src/main/java/mavenProject/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package mavenProject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//TestNg xml from Maven
+	//How to execute all test cases from Test Folder - mvn test
+	@Test
+	public void BrowserAutomation() {
+		
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi() {
+		
+		System.out.println("elementsUi");	
+	}
+
+}//END.
